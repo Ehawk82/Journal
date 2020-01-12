5d13d03a2c0a1b9a51cd31447d5c9e67c45eca44
Jason Graziano <mrehawk82@gmail.com>
2020-01-12 00:59:04 -0500

    ...

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 var myUI, jdata;
-
-
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+var day = ["0","1st","2nd","3rd","4th","5th","6th","7th","8th","9th","10th","11th","12th","13th","14th","15th","16th","17th","18th","19th","20th","21st","22nd","23rd","24th","25th","26th","27th","28th","29th","30th","31st"];
 jdata = {
 	timestamp: 0,
 	collections: {},
@@ -17,20 +18,22 @@ jdata = {
 
 myUI = {
 	init: function(){
-		var jData = parseLS("jData");
+		var jData = parseLS("jData"),
+			d = new Date();
+
 		if(!jData || jData === null) {
 			LSinit("jData", jdata);
 			location.reload();
 		} else {
-			myUI.mainLoadout(jData);
+			myUI.mainLoadout(jData,d);
 		}
 	},
-	mainLoadout: function(jData){
+	mainLoadout: function(jData,d){
 		var startMenu = createEle("div"),
 			startBtn = createEle("button");
 
 		startBtn.innerHTML = "▶️";
-		startBtn.onclick = myUI.startProgram(jData,startMenu);
+		startBtn.onclick = myUI.startProgram(jData,startMenu,d);
 
 		startMenu.className = "startMenu";
 		startMenu.append(startBtn);
@@ -41,12 +44,19 @@ myUI = {
 			makeFull(startMenu);
 		},100);
 	},
-	startProgram: function(jData,startMenu){
+	startProgram: function(jData,startMenu,d){
 		return function(){
+			var wkDay = d.getDay(),
+				mnths = d.getMonth(),
+				dy = d.getDate(),
+				yr = d.getFullYear(); 
+
 			var toolHolder = createEle("div"),
 				homePage = createEle("div"),
-				ofs = createEle("button");
-				cfs = createEle("button");
+				ofs = createEle("button"),
+				cfs = createEle("button"),
+				dailyGrid = createEle("div"),
+				txArea = createEle("textarea");
 
 			startMenu.remove();
 
@@ -62,8 +72,13 @@ myUI = {
 
 			toolHolder.append(ofs,cfs);
 
-			homePage.innerHTML = "text and stuff";
+			txArea.placeholder = "Journal entry for " + days[wkDay] + ", " + months[mnths] + " " + day[dy] + ", " + yr + ".";
+
+			dailyGrid.className = "dailyGrid";
+			dailyGrid.append(txArea);
+
 			homePage.className = "homePage";
+			homePage.append(dailyGrid);
 
 			body.append(toolHolder,homePage);
 
